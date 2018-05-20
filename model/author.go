8d842c26114474ba41b4798a3b52ca3d5ce8aa6e
Nghia Tran <vinhnghiatran@gmail.com>
2018-05-20 21:49:03 +0800

    [ARTIFY-14] Fix json model and fetch associated models

--- model/author.go
+++ model/author.go
@@ -4,10 +4,10 @@ import "github.com/jinzhu/gorm"
 
 type Author struct {
 	gorm.Model
-	Name        string
-	Born        string
-	Died        string
-	Nationality string
-	Wikipedia   string
-	Photos      []Photo
+	Name        string  `json:"name"`
+	Born        string  `json:"born"`
+	Died        string  `json:"died"`
+	Nationality string  `json:"nationality"`
+	Wikipedia   string  `json:"wikipedia"`
+	Photos      []Photo `json:"photos"`
 }
--- resources/photo.go
+++ resources/photo.go
@@ -6,10 +6,11 @@ import (
 
 func (r *Resource) GetLatestFeaturePhoto() (*model.Photo, error) {
 	lastPhoto := model.Photo{}
-	err := r.PostgreSQL.Last(&lastPhoto).Error
+	err := r.PostgreSQL.Last(&lastPhoto).Related(&lastPhoto.Author).Error
 
 	if lastPhoto.ID == 0 {
 		return nil, err
 	}
+
 	return &lastPhoto, nil
 }
